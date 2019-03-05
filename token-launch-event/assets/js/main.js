a08d9740829514707376f356c70db4317868662f
David Hodge <dhodge.jr229@gmail.com>
2019-03-05 00:18:52 -0500

    Updates

--- token-launch-event/assets/js/main.js
+++ token-launch-event/assets/js/main.js
@@ -121,7 +121,7 @@
         allowParentlinks:true
     });
      // Set the date we're counting down to
-      var countDownDate = new Date("2019-03-08").getTime();
+      var countDownDate = new Date("2019-03-22").getTime();
 
       // Update the count down every 1 second
       var x = setInterval(function() {
